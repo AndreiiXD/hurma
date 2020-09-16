16747cc42a34fcc8d0ceeaa20af607b69e3029cd
andre <andrei2000_2010@mail.ru>
2020-09-16 16:24:24 +0300

    new

--- lb1/src/andre.java
+++ lb1/src/andre.java
@@ -4,19 +4,19 @@ public class andre {
         public static void main(String[] args) {
 
             Scanner in = new Scanner(System.in);
-            System.out.print("Enter 1st val: ");
+            System.out.print("Введите первое значение: ");
             int a = in.nextInt();
-            System.out.print("Enter 2nd val: ");
+            System.out.print("Введите второе значение: ");
             int b = in.nextInt();
             in.close();
             System.out.println("__________________");
-            System.out.println("Sum = " + (a + b));
-            System.out.println("Product = " + a * b);
-            System.out.println("Difference = " + (a - b));
+            System.out.println("Сумма = " + (a + b));
+            System.out.println("Умножение = " + a * b);
+            System.out.println("Разница = " + (a - b));
             if (b == 0) {
-                System.out.println("2nd value is null or 0");
+                System.out.println("Второе значение равно 0");
             } else {
-                System.out.println("Division = " + a / b);
+                System.out.println("Деление = " + a / b);
             }
         }
     }
